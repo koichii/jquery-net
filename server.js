a9ea044a606c37384c0ecac78e80bdac5d645639
koichii <koichii@live.jp>
2017-03-25 17:35:13 +0900

    update

--- server.js
+++ server.js
@@ -10,67 +10,67 @@ app.set('view engine', 'ejs');
 var	rooms = {demo: true};
 
 app.get('/', function(req, res){
-  //res.sendFile(__dirname + '/index.html');
-  res.render('index', {rooms: rooms});
+	//res.sendFile(__dirname + '/index.html');
+	res.render('index', {rooms: rooms});
 });
 
 app.get('/client', function(req, res){
-  res.sendFile(__dirname + '/views/client.html');
+	res.sendFile(__dirname + '/views/client.html');
 /*
 	var name = req.query.name;
 	var room = req.query.room;
-  console.log('name: '+ name);
-  console.log('room: '+ room);
-  res.render('client', { name: name, room: room });
+	console.log('name: '+ name);
+	console.log('room: '+ room);
+	res.render('client', { name: name, room: room });
 */
 });
 
 app.get('/admin', function(req, res){
-  res.render('admin');
+	res.render('admin');
 });
 
 io.on('connection', function(socket){
-  // connected
-  socket.emit('connected', socket.id);
-  console.log('connected', socket.id);
+	// connected
+	socket.emit('connected', socket.id);
+	console.log('connected', socket.id);
 
-  // join the room
-  function joinRoom(room){
-    socket.join(room);
-    socket.emit('joind', room);
-    console.log('joind', room);
-    //console.log(io.nsps["/"].adapter.rooms);
-  }
-  // client trys to join the room
+	// join the room
+	function joinRoom(room){
+		socket.join(room);
+		socket.emit('joind', room);
+		console.log('joind', room);
+		//console.log(io.nsps["/"].adapter.rooms);
+	}
+	// client trys to join the room
 	socket.on('join', function(room) {
 		if ( rooms[room] ) {
-      joinRoom(room);
-    } else {
-      console.log('Cannot join:', room);
+			joinRoom(room);
+		} else {
+			console.log('Cannot join:', room);
 		}
-  });
-  // admin trys to create the room
-  socket.on('admin-createRoom', function(room) {
-  	if ( rooms[room] ) {
-      console.log('Cannot creates:', room);
-    } else {
-      rooms[room] = {socket: socket};
-      console.log('createed:', room);
-      joinRoom(room);
-  	}
-  });
-  // script recive and send {from:, to:, script}
-  socket.on('script', function(s) {
-    io.to(s.to).emit('script', s);
-    console.log('script:', s);
-  });
-  // disconnected
-  socket.on('disconnect', function(){
-    console.log('user '+ socket.id +' disconnected');
-  });
+	});
+	// admin trys to create the room
+	socket.on('admin-createRoom', function(room) {
+		if ( rooms[room] ) {
+			console.log('Cannot creates:', room);
+		} else {
+			rooms[room] = {socket: socket};
+			console.log('createed:', room);
+			joinRoom(room);
+		}
+	});
+	// script recive and send {from:, to:, script}
+	socket.on('script', function(s) {
+		io.to(s.to).emit('script', s);
+		console.log('script:', s);
+	});
+	// disconnected
+	socket.on('disconnect', function(){
+		console.log('user '+ socket.id +' disconnected');
+	});
 
 });
 
 http.listen(port, function(){
-  console.log('listening on *:' + port);
+	console.log('listening on *:' + port);
 });
